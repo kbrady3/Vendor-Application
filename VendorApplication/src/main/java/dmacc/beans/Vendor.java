1404a57ec0fbab4edb872718cc51031da1546cbf
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-27 19:01:51 -0500

    Database set up, home page set up

---
+++ VendorApplication/src/main/java/dmacc/beans/Vendor.java
@@ -0,0 +1,50 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vendor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private String address;
+	
+	public Vendor() {
+		super();
+	}
+	public Vendor(String name, String description, String address) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.address = address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
